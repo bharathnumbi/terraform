802d5db3b575fb803f2b0ac8236f3bf35d987844
bharathnumbi <94038636+bharathnumbi@users.noreply.github.com>
2023-05-22 13:02:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "Amazon Linux 2023 AMI 2023.0.20230503.0" {
-  instance_type = "t3.micro"
-  ami = "ami-03574e0a8d917a1c8"
+  instance_type = "t2.micro"
+  ami = "ami-0b08bfc6ff7069aff"
 }
 
  
