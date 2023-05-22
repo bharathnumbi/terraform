dd68c6f3345bf6cc316905f412c42acd0a098324
bharathnumbi <94038636+bharathnumbi@users.noreply.github.com>
2023-05-22 12:51:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "Hyderabad"
+  access_key="AKIAYP4J433SCOMDT4ZW"
+  secret_key="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"
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
+resource "aws_instance" "Amazon Linux 2023 AMI 2023.0.20230503.0" {
+  instance_type = "t3.micro"
+  ami = "ami-03574e0a8d917a1c8"
 }
 
-# Create AWS ec2 instance
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami-0b08bfc6ff7069aff
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = var.tag_name
-  }
-}
-
-# Create Elastic IP address
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
+ 
